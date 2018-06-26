134946b2833129cd4023d52dee1f2c5636c60cdf
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-06-26 06:41:11 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,4 @@
-def greeting(name)
-puts "Hi! I'm HAL, what's your name?"
+puts "Hi! I'm HAL, what's your name? "
 name = gets.strip
 greeting(name)
 puts "Hello #{name}. It's nice to meet you."
-end
